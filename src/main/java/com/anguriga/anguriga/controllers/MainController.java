585b8d98422ef8b474ae3007cf55d94ba23839ac
Jonnycp <jonathan-caputo@hotmail.com>
2022-10-26 01:53:15 +0200

    Usate funzioni create nel main

--- src/main/java/com/anguriga/anguriga/controllers/MainController.java
+++ src/main/java/com/anguriga/anguriga/controllers/MainController.java
@@ -1,15 +1,34 @@
 package com.anguriga.anguriga.controllers;
 
+import com.anguriga.anguriga.Main;
+import javafx.event.Event;
 import javafx.fxml.FXML;
+import javafx.scene.Node;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class MainController {
+    //https://stackoverflow.com/questions/34785417/javafx-fxml-controller-constructor-vs-initialize-method
     @FXML
-    protected void versamento() {
-        System.out.println("Versamento");
+    protected void newATM(Event e) {
+        try{
+            Main.startATM(new Stage(), 0);
+        }catch (IOException ex){
+            System.out.println("Impossibile aprire un nuovo ATM");
+            System.out.println(ex.getMessage());
+        }
     }
     @FXML
-    protected void prelievo() {
-        System.out.println("prelievo");
+    protected void versamento(Event e) {
+        Stage currentStage = (Stage)((Node) e.getSource()).getScene().getWindow();
+        Main.startModal(currentStage, "versamento");
+    }
+    @FXML
+    protected void prelievo(Event e) {
+        Stage currentStage = (Stage)((Node) e.getSource()).getScene().getWindow();
+        Main.startModal(currentStage, "prelievo");
     }
     @FXML
     protected void bollettini() {
