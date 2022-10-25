f2c3f98f155eb2979a94c2f6e2df8778642cd6ca
Jonnycp <jonathan-caputo@hotmail.com>
2022-10-26 00:48:16 +0200

    Aggiunta icona

--- src/main/java/com/anguriga/anguriga/Main.java
+++ src/main/java/com/anguriga/anguriga/Main.java
@@ -3,6 +3,7 @@ package com.anguriga.anguriga;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -15,6 +16,7 @@ public class Main extends Application {
         stage.setTitle("Homepage");
         stage.setResizable(false);
         stage.setScene(scene);
+        stage.getIcons().add(new Image("file:src/main/resources/icon.png"));
         stage.show();
     }
 
