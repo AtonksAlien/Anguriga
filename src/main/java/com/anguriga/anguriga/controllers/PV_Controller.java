3970ce47aa4372b8536940b11006dfde264c7aeb
AtonksAlien <losaccoandrea04@gmail.com>
2022-10-26 13:54:07 +0200

    Realizzato controller Prelievo-Versamento

---
+++ src/main/java/com/anguriga/anguriga/controllers/PV_Controller.java
@@ -0,0 +1,14 @@
+package com.anguriga.anguriga.controllers;
+
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+
+public class PV_Controller implements Initializable {
+    protected Label titolo;
+
+    @Override
+    public void initialize(java.net.URL location, java.util.ResourceBundle resources) {
+        //TODO
+    }
+}
+
