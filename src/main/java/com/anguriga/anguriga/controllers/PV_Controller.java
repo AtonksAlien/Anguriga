6f46e8d6523ceed78dae6b8018ed5447f051df18
Jonnycp <jonathan-caputo@hotmail.com>
2022-10-26 21:24:49 +0200

    Close windows in PV window
    Co-Authored-By: Luca <73708474+Luca-Landri@users.noreply.github.com>

--- src/main/java/com/anguriga/anguriga/controllers/PV_Controller.java
+++ src/main/java/com/anguriga/anguriga/controllers/PV_Controller.java
@@ -1,14 +1,22 @@
 package com.anguriga.anguriga.controllers;
 
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.stage.Stage;
 
 public class PV_Controller implements Initializable {
+    @FXML
     protected Label titolo;
+
+    @FXML
+    private void closeWindow() {
+        ((Stage) titolo.getScene().getWindow()).close();
+    }
 
     @Override
     public void initialize(java.net.URL location, java.util.ResourceBundle resources) {
-        //TODO
+        System.out.println("PV_Controller.initialize");
     }
 }
 
