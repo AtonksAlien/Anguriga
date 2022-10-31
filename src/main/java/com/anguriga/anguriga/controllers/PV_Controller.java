ddc8395314578fd8eac5efd4cdcfd7c9350df5d4
Jonnycp <jonathan-caputo@hotmail.com>
2022-10-31 16:39:48 +0100

    Modificati testi per finestra prelievo/versamento
    in modo dinamico

--- src/main/java/com/anguriga/anguriga/controllers/PV_Controller.java
+++ src/main/java/com/anguriga/anguriga/controllers/PV_Controller.java
@@ -1,35 +1,47 @@
 package com.anguriga.anguriga.controllers;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
-public class PV_Controller {
-    private final String[][] dbTesti = new String[][]{
-            new String[]{"Versamento", "Inserisci la somma da versare", "VERSA"},
-            new String[]{"Prelievo", "Inserisci la somma da prelevare", "PRELEVA"},
-    };
+import java.util.Arrays;
+
+public class PV_Controller implements BorderlessModal{
     private String[] testi;
 
     @FXML
     protected Label titolo;
+    @FXML
+    protected TextField importo;
+    @FXML
+    protected Button button;
 
     @FXML
-    private void closeWindow() {
+    public void closeWindow() {
         ((Stage) titolo.getScene().getWindow()).close();
     }
 
     @FXML
     public void initialize() {
         titolo.setText(testi[0]);
+        importo.setPromptText(testi[1]);
+        button.setText(testi[2]);
     }
 
     public PV_Controller(String titolo){
+        String[][] dbTesti = new String[][]{
+                new String[]{"Versamento", "Inserisci la somma da versare", "VERSA"},
+                new String[]{"Prelievo", "Inserisci la somma da prelevare", "PRELEVA"},
+        };
+
         if(titolo.equalsIgnoreCase("versamento")) {
             testi = dbTesti[0];
         }else if(titolo.equalsIgnoreCase("prelievo")){
             testi = dbTesti[1];
         }
     }
+
 }
 
