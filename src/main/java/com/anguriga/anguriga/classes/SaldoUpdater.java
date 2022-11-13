0277b6734a1599d3a7f10d2d576982398decd8e0
Jonnycp <jonathan-caputo@hotmail.com>
2022-11-13 13:45:29 +0100

    Nuova transazione ogni update

--- src/main/java/com/anguriga/anguriga/classes/SaldoUpdater.java
+++ src/main/java/com/anguriga/anguriga/classes/SaldoUpdater.java
@@ -1,14 +1,20 @@
 package com.anguriga.anguriga.classes;
 
+import javafx.scene.layout.FlowPane;
+
+import java.util.GregorianCalendar;
+
 public class SaldoUpdater implements Runnable {
     private BankAccount conto;
     private String tipo;
     private double importo;
+    private FlowPane transazioni;
 
-    public SaldoUpdater(BankAccount conto, String tipo, double importo) {
+    public SaldoUpdater(BankAccount conto, String tipo, double importo, FlowPane transazioni) {
         setConto(conto);
         setTipo(tipo);
         setImporto(importo);
+        setTransazioni(transazioni);
     }
 
     private void setConto(BankAccount conto) {
@@ -43,12 +49,23 @@ public class SaldoUpdater implements Runnable {
         }
     }
 
+    private void setTransazioni(FlowPane transazioni){
+        if(transazioni != null){
+            this.transazioni = transazioni;
+        }else{
+            System.out.println("Devi avere un contenitore per le transazioni");
+            System.exit(0);
+        }
+    }
+
     @Override
     public void run() {
         if(tipo.equals("versamento")) {
             conto.deposito(importo);
+            new Transazione(importo, "Versamento", new GregorianCalendar()).addTransazione(transazioni);
         } else {
             conto.prelievo(importo);
+            new Transazione(importo, "Prelievo", new GregorianCalendar()).addTransazione(transazioni);
         }
     }
 }
